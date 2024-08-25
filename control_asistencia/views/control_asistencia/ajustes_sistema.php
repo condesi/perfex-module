1ec004732ba6c6d8cdfc76b2d7cd7ac22172518b
Percy Rojas <percy1085@gmail.com>
2024-08-25 17:58:21 -0500

    Delete control_asistencia/views/control_asistencia directory

--- control_asistencia/views/control_asistencia/ajustes_sistema.php
+++
@@ -1,7 +0,0 @@
-<h1>Ajustes del Sistema</h1>
-<form action="<?php echo admin_url('control_asistencia/ajustes_sistema'); ?>" method="post">
-    <label for="ajustes">Ajustes:</label>
-    <textarea id="ajustes" name="ajustes"><?php echo $ajustes; ?></textarea>
-    <br>
-    <button type="submit">Guardar</button>
-</form>
